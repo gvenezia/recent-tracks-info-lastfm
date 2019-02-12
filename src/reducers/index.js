ec4e995670939880c8242e73255d0b9617eafa27
Gaetano <g.venezia@outlook.com>
2019-02-12 09:46:40 -0600

    Add songsReducer and configure

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
+import songsReducer from './songsReducer'
 
 export default combineReducers({
-	placeHolderKey: () => 'placeholderProperty'
+	songs: songsReducer
 });
