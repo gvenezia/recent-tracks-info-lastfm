641c7e8bfe437c0d7ba1844d7ef808729fae6c10
Gaetano <g.venezia@outlook.com>
2019-02-12 14:36:47 -0600

    Add Semantic styled cards for each song in the list

--- src/components/RecentPlaysList.js
+++ src/components/RecentPlaysList.js
@@ -7,12 +7,39 @@ class RecentPlaysList extends Component {
 		this.props.fetchSongs();
 	}
 
+	renderList() {
+		return this.props.songs.map( song => {
+			return (
+				<div key={song.mbid} class="ui card four wide column">
+				  <div class="image">
+				    <img src={song.image[3] ? song.image[3]['#text'] : ''} />
+				  </div>
+				  <div class="content">
+				    <p class="header">"{song.name}"</p>
+				    <div class="meta">
+				      <span class="date">{song.date['#text']}</span>
+				    </div>
+				    <div class="description">
+				      {song.artist['#text']} — {song.album['#text']}
+				    </div>
+				  </div>
+				  <div class="extra content">
+				    <p>
+				      <i class="user icon"> Genres:</i>
+				      22 Friends
+				    </p>
+				  </div>
+				</div>
+			)
+		});
+	}
+
 	render(){
 		console.log(this.props);
 		return (
-			<li>
-				<ul>1</ul>	
-			</li>
+			<div className='ui grid'>
+				{this.renderList()}
+			</div>
 		)
 	}
 }
