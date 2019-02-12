33e3f651ef98b69838c487dd73a1349e0831ea8e
Gaetano <g.venezia@outlook.com>
2019-02-11 21:05:50 -0600

    Refactor action with redux-thunk syntax

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,12 @@
 import axiosLastfm from '../apis/lastfm.js';
 
-export const fetchSongs = async () => {
+// Redux-thunk allows a function to be returned instead of an object
+	// Within the function, we then dispatch the object manually
+export const fetchSongs = () =>  async (dispatch, getState) => {
 	const response = await axiosLastfm.get('?method=user.getrecenttracks&user=grrtano');
 
-	return { 
+	dispatch( { 
 		type: 'FETCH',
 		payload: response
-	}
-}
+	});
+};
