2e7e29d2d15d1e7941156185a9893a269a61f53a
Gaetano <g.venezia@outlook.com>
2019-02-11 21:29:30 -0600

    Refactor axios lastfm request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,14 @@
 import axiosLastfm from '../apis/lastfm.js';
+import { lastfmKeyAndConfig}  from '../apiKeys/lastfm.js';
 
 // Redux-thunk allows a function to be returned instead of an object
 	// Within the function, we then dispatch the object manually
 export const fetchSongs = () =>  async (dispatch, getState) => {
-	const response = await axiosLastfm.get('?method=user.getrecenttracks&user=grrtano');
+	const response = await axiosLastfm.get(
+		'?method=user.getrecenttracks&user=grrtano' + lastfmKeyAndConfig
+	);
+
+	console.log(response.data);
 
 	dispatch( { 
 		type: 'FETCH',
--- src/apis/lastfm.js
+++ src/apis/lastfm.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 const axiosLastfm = axios.create({
-	baseURL: 'http://ws.audioscrobbler.com/2.0/'
-
+	baseURL: `http://ws.audioscrobbler.com/2.0/`
 });
-
-axiosLastfm.defaults.headers.common['Authorization'] = process.env.REACT_APP_LASTM_KEY;
 
 export default axiosLastfm;
