ae9eb836041c0e23620085df7a3dd96188909c90
Gaetano <g.venezia@outlook.com>
2019-02-11 19:32:41 -0600

    Add axios configuration

---
+++ src/apis/lastfm.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const axiosLastfm = axios.create({
+	baseURL: 'http://ws.audioscrobbler.com/2.0/',
+	timeout: 500
+
+});
+
+axiosLastfm.defaults.headers.common['Authorization'] = process.env.REACT_APP_LASTM_KEY;
+
+export default axiosLastfm;
