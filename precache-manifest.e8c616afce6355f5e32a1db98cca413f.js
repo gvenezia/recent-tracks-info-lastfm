ed7bef4da528ede2c0fc28d976d14837750d1428
Gaetano <g.venezia@outlook.com>
2019-06-18 15:12:16 -0700

    Updates

---
+++ precache-manifest.e8c616afce6355f5e32a1db98cca413f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a749bedeab9f69925001dea6ac837c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc7290a5ce9f8c894ac2",
+    "url": "/static/js/2.68482b47.chunk.js"
+  },
+  {
+    "revision": "9e90b60b685fcf0d86f2",
+    "url": "/static/js/main.6b722c5b.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
